55392aae5f383642aa19ec091658d7039bf8438b
shorinbonsai <shorinbonsai@gmail.com>
2018-07-15 09:17:51 -0400

    third level with oscillation initial

---
+++ Assets/Oscillation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillation : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
